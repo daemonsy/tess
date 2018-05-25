6df736063794a691ca20e592ebcb4305224fa7da
Damon Aw <daemonsy@gmail.com>
2018-05-24 22:55:44 -0400

    DRY the db config

--- database/config.js
+++ database/config.js
@@ -1,44 +1,26 @@
-const { PG_USERNAME, PG_PASSWORD } = process.env;
+const { PG_USERNAME, PG_PASSWORD, NODE_ENV } = process.env;
+
+const defaultConfig = {
+  client: 'postgresql',
+  connection: {
+    host: 'localhost',
+    port: 5432,
+    database: `tess_${NODE_ENV || 'development'}`,
+    user: PG_USERNAME,
+    password: PG_PASSWORD
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  },
+  pool: {
+    min: 2,
+    max: 5
+  }
+}
 
 module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: `tess_development`,
-      user: PG_USERNAME,
-      password: PG_PASSWORD
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: `tess_staging`,
-      user: PG_USERNAME,
-      password: PG_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: `tess_production`,
-      user: PG_USERNAME,
-      password: PG_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  development: { ...defaultConfig },
+  test: { ...defaultConfig },
+  staging: { ...defaultConfig },
+  production: { ...defaultConfig }
 };
